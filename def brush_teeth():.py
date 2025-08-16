ae6ede7385151bd70bef1fe2379305f5dad2b921
angelmaesinatao0 <angelmaemutia16@gmail.com>
2025-08-16 15:49:43 +0800

    pseudocode

--- def brush_teeth():.py
+++ def brush_teeth():.py
@@ -4,27 +4,28 @@ def brush_teeth():
 	toothbrush = True
 
 
-    have_toothpaste = False
+	have_toothpaste = False
 
 
-    if have_toothpaste:
-        toothpaste = True
-	    print("Toothpaste added to toothbrush.")
-	else:
-		toothpaste = True
-	    print("Bought toothpaste and added to toothbrush.")
+	if have_toothpaste:
+	toothpaste = True
+	print("Toothpaste added to toothbrush.")
+else:
+	toothpaste = True
+	print("Bought toothpaste and added to toothbrush.")
 
 
 	print("Brushing teeth...")
 
-    print("Gargling water...")
+
+	print("Gargling water...")
 
 
 	print("Spitting water...")
 
 
-    towel = True
+	towel = True
 	print("Wiping mouth with towel.")
 
 
-																									
+    brush_teeth()
